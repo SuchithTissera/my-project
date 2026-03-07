3594c710bcdcb31712fde6e459360125b43fefc2
SuchithTissera <suchithtissera@gmail.com>
2026-03-07 17:58:37 +0530

    Added mathamatical operations python file

---
+++ maths.py
@@ -0,0 +1,47 @@
+#You can do basic addition,substraction, multiplication and division
+#def loops to do the operations
+def addition():
+    global value
+    z = int(input("input the other number"))
+    value += z
+
+def substraction():
+    global value
+    z = int(input("input the other number"))
+    value -= z
+
+def multiplication():
+    global value
+    z = int(input("input the other number"))
+    value = value * z
+
+def division():
+    global value
+    z = int(input("input the other number"))
+    value = value / z
+
+value = 0
+x = int(input("input a number?"))
+value += x
+y = input("what operation do u want Eg : (+,-, *, /)")
+
+#while loop to carry out operations until the user is finished
+finished = 'no'
+while finished == 'no':
+    #if loops to see which operations need to be done
+    if y == '+':
+        addition()
+    elif y == '-':
+        substraction()
+    elif y == '*':
+        multiplication()
+    elif y== '/':
+        division()
+
+    #a output to see the subtotals after each iteration
+    print("current value:", value)
+
+    finished = input("are you done? (yes/no): ")
+
+#the final total 
+print("final value:",value)
