2fe9830f8ebe4b0df4e4440822b03020124df59e
Harindu Udugama <harin@interactik.com>
2026-03-08 09:04:41 +0530

    Update maths.py
    Version 2 of suchith's file

--- maths.py
+++ maths.py
@@ -1,48 +1,79 @@
-#You can do basic addition,substraction, multiplication and division
-#def loops to do the operations
-def addition():
-    global value
-    z = int(input("input the other number"))
-    value += z
-
-def substraction():
-    global value
-    z = int(input("input the other number"))
-    value -= z
-
-def multiplication():
-    global value
-    z = int(input("input the other number"))
-    value = value * z
-
-def division():
-    global value
-    z = int(input("input the other number"))
-    value = value / z
-
-value = 0
-x = int(input("input a number?"))
-value += x
+import math
 
 
-#while loop to carry out operations until the user is finished
-finished = 'no'
-while finished == 'no':
-    y = input("what operation do u want Eg : (+,-, *, /)")
-    #if loops to see which operations need to be done
-    if y == '+':
-        addition()
-    elif y == '-':
-        substraction()
-    elif y == '*':
-        multiplication()
-    elif y== '/':
-        division()
+def addition(value):
+    number = float(input("Enter the next number: "))
+    return value + number
 
-    #a output to see the subtotals after each iteration
-    print("current value:", value)
 
-    finished = input("are you done? (yes/no): ")
+def subtraction(value):
+    number = float(input("Enter the next number: "))
+    return value - number
 
-#the final total 
-print("final value:",value)
+
+def multiplication(value):
+    number = float(input("Enter the next number: "))
+    return value * number
+
+
+def division(value):
+    number = float(input("Enter the next number: "))
+    if number == 0:
+        print("Error: Cannot divide by zero.")
+        return value
+    return value / number
+
+
+def power(value):
+    number = float(input("Enter the exponent: "))
+    return value ** number
+
+
+def modulus(value):
+    number = float(input("Enter the next number: "))
+    if number == 0:
+        print("Error: Cannot do modulus by zero.")
+        return value
+    return value % number
+
+
+def square_root(value):
+    if value < 0:
+        print("Error: Cannot find square root of a negative number.")
+        return value
+    return math.sqrt(value)
+
+
+print("Simple Calculator")
+print("Available operations: +  -  *  /  ^  %  sqrt  reset  exit")
+
+value = float(input("Enter the starting number: "))
+
+while True:
+    operation = input("Choose an operation: ").lower()
+
+    if operation == "+":
+        value = addition(value)
+    elif operation == "-":
+        value = subtraction(value)
+    elif operation == "*":
+        value = multiplication(value)
+    elif operation == "/":
+        value = division(value)
+    elif operation == "^":
+        value = power(value)
+    elif operation == "%":
+        value = modulus(value)
+    elif operation == "sqrt":
+        value = square_root(value)
+    elif operation == "reset":
+        value = float(input("Enter a new starting number: "))
+    elif operation == "exit":
+        break
+    else:
+        print("Invalid operation. Please try again.")
+        continue
+
+    print("Current value:", value)
+
+print("Final value:", value)
